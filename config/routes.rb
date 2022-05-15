a9e6e753367f18d94794a0b2ac2dfe434e928508
重光青空 <shigemitsuaozora@Sora.local>
2022-05-16 01:10:49 +0900

    homepe-zi

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  
+  get 'static_pages/home'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
