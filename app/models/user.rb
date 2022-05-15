36292430711e3a09074b3c47e0cf7af85ceda6da
重光青空 <shigemitsuaozora@Sora.local>
2022-05-16 04:59:06 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,5 +17,5 @@ class User < ApplicationRecord
   # 仮想的な属性passwordとpassword_confirmationが使える また存在性と値が一致するかのバリデーションも追加される
   # authenticatedメソッドが使える
   has_secure_password
-  validates :password, presence :true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 }
 end
