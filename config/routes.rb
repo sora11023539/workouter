43806614dab9a4a23ddae48bb1c5dd94dc9f7078
重光青空 <shigemitsuaozora@Sora.local>
2022-05-16 04:12:00 +0900

    アカウント登録ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
+  
   get 'static_pages/home'
-
+  get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
+
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 end
