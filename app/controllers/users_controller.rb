27a0a4d5892c3a8936ccf99c5e87f333cf1949b0
重光青空 <shigemitsuaozora@Sora.local>
2022-05-17 23:23:14 +0900

    ユーザー一覧ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
   def show
@@ -33,6 +33,10 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def index
+    @users = User.all
   end
 
   private
