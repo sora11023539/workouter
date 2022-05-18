06ac0670993fda527320705b97836d30bee46ae3
重光青空 <shigemitsuaozora@Sora.local>
2022-05-19 04:49:07 +0900

    パスワード再設定期限切れ確認

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,8 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  # パス再設定の有効期限が切れていないか
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -24,7 +26,29 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    # 新しいパスワードが空になっていないか
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    # 新しいパスが正しければ更新する
+    elsif @user.update(user_params)
+      log_in @user
+      # 再設定完了したらnilにする
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "パスワードが変更されました"
+      redirect_to @user
+    # 無効なパスであれば失敗
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
 
     def get_user
       @user = User.find_by(email: params[:email])
@@ -34,6 +58,14 @@ class PasswordResetsController < ApplicationController
     def valid_user
       unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
+      end
+    end
+
+    # トークンが期限切れか
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "パスワード再設定の有効期限が切れました。"
+        redirect_to new_password_reset_url
       end
     end
 
