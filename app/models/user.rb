899a4182cc8bcdbd4a5132cd33a22cb212c00292
重光青空 <shigemitsuaozora@Sora.local>
2022-05-19 06:03:51 +0900

    follow, followerの関係性構築

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # Userモデルにfollowingの関連付けをする
+  # source following配列の元はfollowed id の集合
+  has_many :following, through: :active_relationships, source: :followed
+  # Userモデルにfollowersの関連付けをする
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attr_accessor :remember_token, :activation_token, :reset_token
   # saveする前に小文字に変換
   before_save :downcase_email
@@ -84,6 +92,21 @@ class User < ApplicationRecord
   # パス再設定の期限が切れている場合はtrue
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
