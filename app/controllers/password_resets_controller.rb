5184ad519391cbd61617ad62264ef9c361259899
重光青空 <shigemitsuaozora@Sora.local>
2022-05-19 03:52:26 +0900

    password reset routing

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
@@ -10,14 +13,28 @@ class PasswordResetsController < ApplicationController
       @user.create_reset_digest
       # 再設定のメール送信
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
+      flash[:info] = "パスワード再設定メールを送信しました"
       redirect_to root_url
     else
-      flash.now[:danger] = "Email address not found"
+      flash.now[:danger] = "メールアドレスが見つかりません"
       render 'new'
     end
   end
 
   def edit
   end
+
+  private
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # 正しいユーザーかどうか確認
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
 end
