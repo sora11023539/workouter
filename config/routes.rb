bf60979f9ff05fe03a894f0a0efd2fecf266f174
重光青空 <shigemitsuaozora@Sora.local>
2022-05-19 06:37:14 +0900

    followに関するパーシャル追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,16 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  # Usersコントローラーにfollowing, followersアクション追加
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :relationships, only: [:create, :destroy]
 
   if Rails.env.development?
     # http://localhost:3000/letter_opener にアクセスすると送信したメール確認できる
