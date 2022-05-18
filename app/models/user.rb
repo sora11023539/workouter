77cca4e33315efe18b4dff68caef8b79a6fe5d55
重光青空 <shigemitsuaozora@Sora.local>
2022-05-19 01:09:06 +0900

    fix Faker error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
 
   # userがdbに保存される前にname,emailフィールドが存在するか
-  validates :name, presence: true, length: { maximum: 12 }
+  validates :name, presence: true, length: { maximum: 100 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
