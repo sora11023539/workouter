30071fd9268e69fb9e24169756691f5e86b3a4b3
重光青空 <shigemitsuaozora@Sora.local>
2022-05-19 02:14:08 +0900

    password resets controller create

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
   get '/signup', to: 'users#new'
-  get '/login', to: 'sessions#new'
 
+  get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   if Rails.env.development?
     # http://localhost:3000/letter_opener にアクセスすると送信したメール確認できる
