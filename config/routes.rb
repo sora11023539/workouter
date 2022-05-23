74b0d1fcf02b0fd2f0bcbb0e74731486b9776827
重光青空 <shigemitsuaozora@Sora-2.local>
2022-05-23 23:25:33 +0900

    chats-controller create

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  
+
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
+  get 'chat/:id', to: 'chats#show', as: 'chat'
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
@@ -17,6 +18,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :relationships, only: [:create, :destroy]
+  resources :chats, only: [:create]
 
   if Rails.env.development?
     # http://localhost:3000/letter_opener にアクセスすると送信したメール確認できる
