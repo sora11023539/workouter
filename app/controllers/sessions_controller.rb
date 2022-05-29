2d93dc5fc3c3c2c86948a10450620267460f56ed
重光青空 <shigemitsuaozora@Sora-2.local>
2022-05-30 07:55:43 +0900

    change: change redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,12 +11,12 @@ class SessionsController < ApplicationController
       if user.activated?
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
+        redirect_back_or users_url
       else
         message = "アカウントが有効化されていません"
         message += "メールボックスの有効化リンクを確認してください"
         flash[:warning] = message
-        redirect_to root_url
+        redirect_to 'new'
       end
     else
       flash.now[:danger] = 'ログインに失敗'
@@ -27,6 +27,6 @@ class SessionsController < ApplicationController
   # cookieに保存されたuser.idを削除し、ログアウト
   def destroy
     log_out if logged_in?
-    redirect_to root_url
+    render 'new'
   end
 end
