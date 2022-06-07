b3bcc64a8c54188b0fb6238b5c455f6df15a5695
重光青空 <shigemitsuaozora@Sora-2.local>
2022-06-08 02:48:55 +0900

    delete: Delete unnecessary description

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,6 @@ class LikesController < ApplicationController
 
   def create
     @user = User.find(params[:id])
-    # Like.create(user_id: current_user.id)
     Like.create(user_id: @user.id, like_id: current_user.id)
     respond_to do |format|
       format.html { redirect_to @user }
@@ -12,16 +11,10 @@ class LikesController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-    # Like.find_by(user_id: current_user.id).destroy
     Like.find_by(user_id: @user.id, like_id: current_user.id).destroy
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
     end
   end
-
-  def show
-    # @users = User.find_by(id: params[:id])
-  end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated?
-    # @likes_count = Like.where(user_id: @user.id).count
     @likes_count = @user.likes.count
   end
 
