87613cb06ff9befeb4beac7ee1824dcbc1b6689f
重光青空 <shigemitsuaozora@Sora-2.local>
2022-06-10 17:51:59 +0900

    add: add users_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,40 @@
 require 'rails_helper'
 
 # Userモデルのテスト
-describe User do
-  # createメソッドである
-  describe '#create' do
-    it "nameがない場合は登録できないこと" do
-      # user作成 nameは空に
-      user = FactoryBot.build(:user, name: nil)
-      # バリデーションにより保存ができない状態か
-      user.valid?
-      # を入力してください というエラーが出るか
-      expect(user.errors[:name]).to include("を入力してください")
-    end
+RSpec.describe User, type: :model do
+  # ニックネーム,メールアドレス,パスワード（確認）があれば有効であること
+  xit "is valid with a name, email, password, password_confirm" do
+    expect(FactoryBot.create(:user)).to be_vaild
+  end
+
+  # ニックネームがなければ無効な状態であること
+  it 'is invalid without a name' do
+    user = FactoryBot.build(:user, name: nil)
+    expect(user).not_to be_valid
+  end
+
+  # メールアドレスがなければ無効な状態であること
+  it 'is invalid without an email address' do
+    user = FactoryBot.build(:user, email: nil)
+    expect(user).not_to be_valid
+  end
+
+  # 重複したメールアドレスの場合、無効な状態であること
+  it "is invalid with a duplicate email address" do
+    FactoryBot.create(:user, email: "test@test.com")
+    user = FactoryBot.build(:user, email: "test@test.com")
+    expect(user).not_to be_valid
+  end
+
+  # パスワードが8文字以上なければ無効な状態であること
+  it 'is invalid without a password that has less than 8 characters' do
+    user = FactoryBot.build(:user, password: "passwor")
+    expect(user).not_to be_valid
+  end
+
+  # ニックネームがなければ無効な状態であること
+  it 'is invalid without a name' do
+    user = FactoryBot.build(:user, name: nil)
+    expect(user).not_to be_valid
   end
 end
