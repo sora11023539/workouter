f3abec0a5b47bf60fa331b9b8f299ae2b203eb1a
重光青空 <shigemitsuaozora@Sora-2.local>
2022-06-13 17:10:04 +0900

    fix: fix relationship_spec

--- spec/models/relatioship_spec.rb
+++ spec/models/relatioship_spec.rb
@@ -2,29 +2,45 @@ require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
 
-  describe "#create" do
+  describe '#create' do
+    context "If you can save" do
+      # 全てのパラメーターが揃っていれば保存できる
+      it "is have all the parameters, it can save it." do
+        expect(FactoryBot.create(:relationship)).to be_valid
+      end
+    end
+
+    context "If you can't save" do
+      # follower_idがnilの場合は保存できない
+      it "is can't save if follower_id is nil" do
+        expect(FactoryBot.build(:relationship, follower_id: nil)).not_to be_valid
+      end
+
+      # followed_idがnilの場合は保存できない
+      it "is can't save if followed_id is nil" do
+        expect(FactoryBot.build(:relationship, followed_id: nil)).not_to be_valid
+      end
+    end
+
     context "uniqueness verification" do
       before do
         @relation = FactoryBot.create(:relationship)
-        @user = FactoryBot.build(:relationship)
+        @user1 = FactoryBot.build(:relationship)
       end
 
       # follower_idとfollowed_idの組み合わせは一意でなければ保存できない
       it "is the combination of follower_id and followed_id can only be saved if it is unique" do
-        relation2 = FactoryBot.build(:relationship, followed_id: @relation.followed_id, followed_id: @relation.followed_id)
-        expect(relation2).not_to be_vaild
+        expect(FactoryBot.build(:relationship, follower_id: @relation.follower_id, followed_id: @relation.followed_id)).not_to be_valid
       end
 
       # follower_idが同じでもfollowed_idが違うなら保存できる
-      it "is even if the follower_id is the same, it can be saved if the followed_id is different." do
-        relation2 = FactoryBot.build(:relationship, follower_id: @relation.follower_id, followed_id: @user.followed_id)
-        expect(relation2).to be_valid
+      it "is even if the follower_id is the same, it can be saved if the followed_id is different" do
+        expect(FactoryBot.build(:relationship, follower_id: @relation.follower_id, followed_id: @user1.followed_id)).to be_valid
       end
 
       # follower_idが違うならfollowed_idが同じでも保存できる
       it "is if the follower_id is different, you can save even if the followed_id is the same" do
-        relation2 = FactoryBot.build(:relationship, follower_id: @user.follower_id, followed_id: @relation.followed_id)
-        expect(relation2).to be_valid
+        expect(FactoryBot.build(:relationship, follower_id: @user1.follower_id, followed_id: @relation.followed_id)).to be_valid
       end
     end
   end
